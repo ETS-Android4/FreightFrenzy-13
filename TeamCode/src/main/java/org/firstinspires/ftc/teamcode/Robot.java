b877343e0517c43a8be7f7db6555a1c968a9f231
qwertyuioplkjhgfd <qwertyuioplkjhgfd@users.noreply.github.com>
2021-11-06 16:09:02 -0700

    bad code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,27 +1,108 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+import java.util.Iterator;
+import java.util.List;
+
+import minipid.MiniPID;
 
 public class Robot {
     public DcMotor frontLeft, frontRight, backLeft, backRight;
+    public DcMotor extender;
+    public Servo grabber;
+    public CRServo spinner;
+    public BNO055IMU imu;
 
-    private HardwareMap hwMap;
+    private LinearOpMode opMode;
+    private MiniPID pid, rpid;
 
-    public Robot(OpMode opMode) {
-        hwMap = opMode.hardwareMap;
+    private static final String VUFORIA_KEY =
+            "AV1AlC//////AAABmZ8844uwbEHrg1LUsHVFKxlStW4C7oPMwyIXaVB2lFgVrXI7AcN37g06/oHM+7Smo0UtpZXtGANu2IWFTeqOdHO83zy8s3nw7ZfZ60OUz9L230sZ0liJbP8aeIKa0a0ibeL+mH4zJTOHU/3rdfcv8PbufYdeMh1ImaoFXTXQkMqiELuxK32/kvH/sRyvMg5JmoQDxKgSgNhN/Vle754F6hCOVk1alZE7H5gXHifhPtL0Gf+AhkrfsbKi+zeZ3gRoGLzX54Qq8EUmOhlm5+ZMbdYkx1F4u8FoLczDK+Qt4J23kEqkbCA5HyDJJsmyA30/fEIYDEepO9f86U96LfOCIFt8Q3vFCWSq4IJZphMlVOF7";
+    private VuforiaLocalizer vuforia;
+    private TFObjectDetector tfod;
 
-        frontLeft = hwMap.get(DcMotor.class, "frontLeft");
-        frontRight = hwMap.get(DcMotor.class, "frontRight");
-        backLeft = hwMap.get(DcMotor.class, "backLeft");
-        backRight = hwMap.get(DcMotor.class, "backRight");
+    public Robot(LinearOpMode opMode) {
+        this.opMode = opMode;
+
+        initPID();
+        initMotors();
+        initIMU();
+        initVuforia();
+        initTfod();
+    }
+
+    private void initPID() {
+        pid = new MiniPID(1, 0, 0);
+        pid.setOutputLimits(-1.0, 1.0);
+        rpid = new MiniPID(1, 0, 0);
+
+    }
+
+    private void initMotors() {
+        frontLeft = opMode.hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = opMode.hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = opMode.hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = opMode.hardwareMap.get(DcMotor.class, "backRight");
 
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.REVERSE);
     }
 
+    private void initIMU() {
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        imu = opMode.hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+    }
+
+    private void initTfod() {
+        int tfodMonitorViewId = opMode.hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", opMode.hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.8f;
+        tfodParameters.isModelTensorFlow2 = true;
+        tfodParameters.inputSize = 320;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset("FreightFrenzy_BCDM.tflite", "Ball", "Cube", "Duck", "Marker");
+        tfod.activate();
+    }
+
+    private void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraName = opMode.hardwareMap.get(WebcamName.class, "Webcam 1");
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
+    }
+
+    /**
+     * drive power only
+     */
     public void setPower(double power) {
         frontLeft.setPower(power);
         frontRight.setPower(power);
@@ -29,5 +110,82 @@ public class Robot {
         backRight.setPower(power);
     }
 
+    public void setPower(double fl, double fr, double bl, double br) {
+        frontLeft.setPower(fl);
+        frontRight.setPower(fr);
+        backLeft.setPower(bl);
+        backRight.setPower(br);
+    };
 
+    public void travelFor(long millis) {
+        travelFor(millis, 0.75);
+    }
+
+    public void travelFor(long millis, double power) {
+        setPower(power);
+        opMode.sleep(millis);
+        setPower(0);
+    }
+
+    public void travelPID(double inches) {
+        pid.reset();
+        int steps = (int)inches * 1000;
+        int old = currentPosition();
+        int goal = old + steps;
+
+        while(Math.abs(currentPosition() - goal) > 10 && opMode.opModeIsActive()) {
+            int diff = currentPosition() - goal;
+
+            double output = pid.getOutput(diff, goal);
+            setPower(output);
+            opMode.sleep(50);
+        }
+        long until = System.currentTimeMillis() + 500;
+        while(System.currentTimeMillis() < until && opMode.opModeIsActive()) {
+            int diff = currentPosition() - goal;
+
+            double output = pid.getOutput(diff, goal);
+            setPower(output);
+            opMode.sleep(50);
+        }
+    }
+
+    private int currentPosition() {
+        return (frontLeft.getCurrentPosition() + frontRight.getCurrentPosition() + backLeft.getCurrentPosition() + backRight.getCurrentPosition())/4;
+    }
+
+    private double currentHeading() {
+        return imu.getAngularOrientation().firstAngle;
+    }
+
+    public void turn(double degrees) {
+        double goal = currentHeading() + degrees;
+        rpid.reset();
+
+        while(Math.abs(currentHeading() - goal) > 10 && opMode.opModeIsActive()) {
+            double diff = currentPosition() - goal;
+
+            double output = pid.getOutput(diff, goal);
+            setPower(output, -output, output, -output);
+            opMode.sleep(50);
+        }
+        long until = System.currentTimeMillis() + 500;
+        while(System.currentTimeMillis() < until && opMode.opModeIsActive()) {
+            double diff = currentPosition() - goal;
+
+            double output = pid.getOutput(diff, goal);
+            setPower(output, -output, output, -output);
+            opMode.sleep(50);
+        }
+    }
+
+    public void recognize() {
+        List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+        for (Recognition recognition : updatedRecognitions) {
+            if(recognition.getLabel().equals("hi")) {
+                double angle = recognition.estimateAngleToObject(AngleUnit.DEGREES);
+            }
+
+        }
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DriverControlled.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DriverControlled.java
@@ -27,8 +27,40 @@ public class DriverControlled extends LinearOpMode {
         runtime.reset();
 
         while (opModeIsActive()) {
-            double drive = -gamepad1.left_stick_y;
-            robot.setPower(drive);
+            double drive = -gamepad1.left_stick_y-gamepad1.right_stick_y;
+            double turn = gamepad1.left_stick_x;
+            double strafe = gamepad1.right_stick_x;
+            robot.setPower(drive+turn-strafe, drive-turn+strafe, drive+turn+strafe, drive-turn-strafe);
+
+            double extend = 0.0;
+            if(gamepad1.dpad_up) {
+                extend = 1.0;
+            } else if(gamepad1.dpad_down) {
+                extend = -1.0;
+            }
+            robot.extender.setPower(extend);
+
+            double grab = 0.0;
+            boolean flag = true;
+            if(gamepad1.dpad_right) {
+                grab = 0.5;
+            } else if(gamepad1.dpad_left) {
+                grab = 1.0;
+            } else {
+                flag = false;
+            }
+            if(flag) {
+                robot.grabber.setPosition(grab);
+            }
+
+            double spin = 0.0;
+            if(gamepad1.right_bumper) {
+                spin = 1.0;
+            } else if(gamepad1.left_bumper) {
+                spin = -1.0;
+            }
+            robot.spinner.setPower(spin);
+
         }
     }
 }
