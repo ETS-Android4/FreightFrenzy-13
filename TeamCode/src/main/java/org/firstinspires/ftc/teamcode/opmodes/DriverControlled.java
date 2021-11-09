8c2eb8cdb147b8b37a8d2dfd945080a41c6d0060
DolphinGui <umeda.shin@gmail.com>
2021-11-08 17:03:59 -0800

    reformatted some stuff.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DriverControlled.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/DriverControlled.java
@@ -2,9 +2,6 @@ package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.Robot;
 
@@ -12,54 +9,45 @@ import org.firstinspires.ftc.teamcode.Robot;
 public class DriverControlled extends LinearOpMode {
 
     // Declare OpMode members.
-    private ElapsedTime runtime = new ElapsedTime();
-    private Robot robot;
+    private final Robot robot = new Robot(this);
 
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-
-        robot = new Robot(this);
-
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
-        runtime.reset();
-
         while (opModeIsActive()) {
-            double drive = -gamepad1.left_stick_y-gamepad1.right_stick_y;
+            double drive = -gamepad1.left_stick_y - gamepad1.right_stick_y;
             double turn = gamepad1.left_stick_x;
             double strafe = gamepad1.right_stick_x;
-            robot.setPower(drive+turn-strafe, drive-turn+strafe, drive+turn+strafe, drive-turn-strafe);
+            robot.setPower(
+                    drive + turn - strafe,
+                    drive - turn + strafe,
+                    drive + turn + strafe,
+                    drive - turn - strafe);
 
-            double extend = 0.0;
-            if(gamepad1.dpad_up) {
-                extend = 1.0;
-            } else if(gamepad1.dpad_down) {
-                extend = -1.0;
-            }
-            robot.extender.setPower(extend);
+            if (gamepad1.dpad_up) {
+                robot.extender.setPower(1.0);
+            } else if (gamepad1.dpad_down) {
+                robot.extender.setPower(-1.0);
+            } else
+                robot.extender.setPower(0.0);
 
-            double grab = 0.0;
-            boolean flag = true;
-            if(gamepad1.dpad_right) {
-                grab = 0.5;
-            } else if(gamepad1.dpad_left) {
-                grab = 1.0;
-            } else {
-                flag = false;
-            }
-            if(flag) {
-                robot.grabber.setPosition(grab);
+
+            if (gamepad1.dpad_right) {
+                robot.grabber.setPosition(0.5);
+            } else if (gamepad1.dpad_left) {
+                robot.grabber.setPosition(1.0);
             }
 
-            double spin = 0.0;
-            if(gamepad1.right_bumper) {
-                spin = 1.0;
-            } else if(gamepad1.left_bumper) {
-                spin = -1.0;
-            }
-            robot.spinner.setPower(spin);
+            if (gamepad1.right_bumper) {
+                robot.spinner.setPower(1.0);
+            } else if (gamepad1.left_bumper) {
+                robot.spinner.setPower(-1.0);
+            } else
+                robot.spinner.setPower(0);
+
 
         }
     }
